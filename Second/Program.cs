1e1a96a178ce6f8d6201b4a4ba5023bd8e31fc2c
Amil Mammadov <amil.mammadov@simbrella.com>
2021-04-19 17:49:37 +0400

    CS file added.

---
+++ Second/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is second part of git projects testing.");
+            Console.ReadLine();
+        }
+    }
+}
